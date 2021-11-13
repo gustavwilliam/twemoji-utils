74e79fa0f31134bd0ae2462e4e4344b254fb1ff1
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2021-11-13 04:13:48 +0100

    Adds Grid

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,108 @@
       { name: 'Calendar', href: '#', current: false },
     ]"
   />
+  <Grid
+    :products="[
+      {
+        id: 1,
+        codepoint: '1F604',
+        name: 'Grinning face with smiling eyes',
+        href: '#',
+        imageSrc:
+          'https://raw.githubusercontent.com/twitter/twemoji/master/assets/svg/1f604.svg',
+        imageAlt: 'Grinning face with smiling eyes.',
+      },
+      {
+        id: 2,
+        codepoint: '1F604',
+        name: 'Grinning face with smiling eyes',
+        href: '#',
+        imageSrc:
+          'https://raw.githubusercontent.com/twitter/twemoji/master/assets/svg/1f604.svg',
+        imageAlt: 'Grinning face with smiling eyes.',
+      },
+      {
+        id: 3,
+        codepoint: '1F604',
+        name: 'Grinning face with smiling eyes',
+        href: '#',
+        imageSrc:
+          'https://raw.githubusercontent.com/twitter/twemoji/master/assets/svg/1f604.svg',
+        imageAlt: 'Grinning face with smiling eyes.',
+      },
+      {
+        id: 4,
+        codepoint: '1F604',
+        name: 'Grinning face with smiling eyes',
+        href: '#',
+        imageSrc:
+          'https://raw.githubusercontent.com/twitter/twemoji/master/assets/svg/1f604.svg',
+        imageAlt: 'Grinning face with smiling eyes.',
+      },
+      {
+        id: 5,
+        codepoint: '1F604',
+        name: 'Grinning face with smiling eyes',
+        href: '#',
+        imageSrc:
+          'https://raw.githubusercontent.com/twitter/twemoji/master/assets/svg/1f604.svg',
+        imageAlt: 'Grinning face with smiling eyes.',
+      },
+      {
+        id: 6,
+        codepoint: '1F604',
+        name: 'Grinning face with smiling eyes',
+        href: '#',
+        imageSrc:
+          'https://raw.githubusercontent.com/twitter/twemoji/master/assets/svg/1f604.svg',
+        imageAlt: 'Grinning face with smiling eyes.',
+      },
+      {
+        id: 7,
+        codepoint: '1F604',
+        name: 'Grinning face with smiling eyes',
+        href: '#',
+        imageSrc:
+          'https://raw.githubusercontent.com/twitter/twemoji/master/assets/svg/1f604.svg',
+        imageAlt: 'Grinning face with smiling eyes.',
+      },
+      {
+        id: 8,
+        codepoint: '1F604',
+        name: 'Grinning face with smiling eyes',
+        href: '#',
+        imageSrc:
+          'https://raw.githubusercontent.com/twitter/twemoji/master/assets/svg/1f604.svg',
+        imageAlt: 'Grinning face with smiling eyes.',
+      },
+      {
+        id: 9,
+        codepoint: '1F604',
+        name: 'Grinning face with smiling eyes',
+        href: '#',
+        imageSrc:
+          'https://raw.githubusercontent.com/twitter/twemoji/master/assets/svg/1f604.svg',
+        imageAlt: 'Grinning face with smiling eyes.',
+      },
+      {
+        id: 10,
+        codepoint: '1F604',
+        name: 'Grinning face with smiling eyes',
+        href: '#',
+        imageSrc:
+          'https://raw.githubusercontent.com/twitter/twemoji/master/assets/svg/1f604.svg',
+        imageAlt: 'Grinning face with smiling eyes.',
+      },
+    ]"
+  />
 </template>
 
 <script>
 import Menu from "./components/Navbar.vue";
+import Grid from "./components/Grid.vue";
 
 export default {
   name: "App",
-  components: { Menu },
+  components: { Menu, Grid },
 };
 </script>
---
+++ src/components/Grid.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="bg-white">
+    <div
+      class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8"
+    >
+      <h2 class="sr-only">Products</h2>
+
+      <div
+        class="
+          grid grid-cols-3
+          gap-y-10
+          sm:grid-cols-5
+          gap-x-6
+          lg:grid-cols-8
+          xl:grid-cols-9 xl:gap-x-8
+        "
+      >
+        <a
+          v-for="product in products"
+          :key="product.id"
+          :href="product.href"
+          class="group"
+        >
+          <div class="w-full aspect-w-1 aspect-h-1 rounded-xl overflow-hidden">
+            <img
+              :src="product.imageSrc"
+              :alt="product.imageAlt"
+              class="
+                w-full
+                h-full
+                object-center object-cover
+                group-hover:opacity-75
+              "
+            />
+          </div>
+          <h3 class="mt-4 text-sm text-gray-700">
+            {{ product.name }}
+          </h3>
+          <p class="mt-1 text-lg font-medium text-gray-900">
+            {{ product.codepoint }}
+          </p>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    products: Array,
+  },
+};
+</script>
