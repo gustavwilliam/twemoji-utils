4c3a25aa36e56b99e9bcdcf221ae240e9944194c
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2021-11-13 18:52:03 +0100

    Use shortened list for emojis
    The previous version was massive. This is temporary, until pagination
    (and search) is completed.

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
 import Menu from "./components/Navbar.vue";
 import Grid from "./components/Grid.vue";
 import Modal from "./components/Modal.vue";
-import emojiData from "./assets/emojidata.json";
+import emojiData from "./assets/emojidata-s.json";
 
 export default {
   name: "App",
