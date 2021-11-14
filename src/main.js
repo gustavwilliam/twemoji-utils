dc28bde4ee7f07603323abb59f0f80c64d372e30
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2021-11-14 18:59:11 +0100

    Integrate basic Vue router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue/dist/vue.esm-bundler'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 import './index.css'
 import './assets/tailwind.css'
 
-createApp(App).mount('#app')
+
+const routes = [
+    { path: '/', component: App },
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+})
+
+
+createApp({})
+    .use(router)
+    .mount('#app')
