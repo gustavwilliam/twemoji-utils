7165901b3f37a41069b6d8ec7cd98672b9679a46
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2021-11-17 15:52:39 +0100

    Add basic 404 page and errormessage component

---
+++ src/components/ErrorMessagePage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="max-w-7xl py-16 px-4 mx-auto sm:px-6 lg:px-8 sm:py-24">
+    <h2 class="text-blue-twitter text-sm uppercase font-semibold">
+      {{ errorCode }} error
+    </h2>
+    <h1 class="text-5xl font-extrabold text-gray-900 dark:text-gray-100 py-3">
+      {{ header }}
+    </h1>
+    <p class="opacity-50 dark:text-white">
+      {{ description }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ErrorMessagePage",
+  props: {
+    errorCode: {
+      type: String,
+      required: false,
+      default: "Unknown",
+    },
+    header: {
+      type: String,
+      required: false,
+      default: "Something went wrong",
+    },
+    description: {
+      type: String,
+      required: false,
+      default: "An unexpected error occurred. Sorry for the inconvenience.",
+    },
+  },
+};
+</script>
--- src/views/Page404.vue
+++ src/views/Page404.vue
@@ -9,14 +9,19 @@
       },
     ]"
   />
-  <h1>404 not found</h1>
+  <ErrorMessagePage
+    errorCode="404"
+    header="Page not found"
+    description="Sorry, but the page you're looking for doesn't seem to exist."
+  />
 </template>
 
 <script>
 import TheNavbar from "@/components/TheNavbar.vue";
+import ErrorMessagePage from "@/components/ErrorMessagePage.vue";
 
 export default {
-  name: "404",
-  components: { TheNavbar },
+  name: "Page404",
+  components: { TheNavbar, ErrorMessagePage },
 };
 </script>
