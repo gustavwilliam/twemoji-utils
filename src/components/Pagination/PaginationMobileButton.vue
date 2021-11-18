44b0496e4cde4338b62dc67478c67ce44512a200
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2021-11-18 23:09:16 +0100

    Add dark mode support to mobile
    And make default dark mode consistent with search bar

--- src/components/Pagination/PaginationMobileButton.vue
+++ src/components/Pagination/PaginationMobileButton.vue
@@ -8,12 +8,16 @@
       px-4
       py-2
       border border-gray-300
+      dark:border-gray-700
       text-sm
       font-medium
       rounded-md
       text-gray-700
+      dark:text-gray-300
       bg-white
+      dark:bg-gray-800
       hover:bg-gray-50
+      dark:hover:bg-gray-700
     "
   >
     {{ content }}
